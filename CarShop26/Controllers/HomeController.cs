57cf862e5ab21bd08522681ece31895ca3e769bc
IliyanSidjimkov <sidjimkov_1979@abv.bg>
2026-02-10 12:26:24 +0200

    Add project files.

---
+++ CarShop26/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using CarShop26.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop26.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
